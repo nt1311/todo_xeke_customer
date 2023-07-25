18cf07a4be34ccf64be16057776cb02d28cb4aad
Ngoc Tuan <hvntuan1311@gmail.com>
2023-07-25 16:27:51 +0700

    routes_1

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,1 +1,13 @@
 part of "app_pages.dart";
+
+abstract class AppRoutes {
+  static const SPLASH = '/';
+  static const ONBOARDINGPAGE = '/onboardingpage';
+  static const LOCATION_ON = '/location on';
+  static const LOGIN_ACCOUNT = '/LOGIN ACCOUNT';
+  static const OTP = '/otp';
+  static const AUTH = "/auth";
+  static const PERSONAL_INFORMATION = '/personal information';
+  static const MAIN_MENU = '/main menu';
+  static const HOME = '/home';
+}
