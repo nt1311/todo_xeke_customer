39e34cee4bff8b120f90a3035bf9386ebaa02f95
Ngoc Tuan <hvntuan1311@gmail.com>
2023-07-25 15:18:51 +0700

    add routes a

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,5 +1,6 @@
 part of "app_pages.dart";
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 abstract class AppRoutes {
   static const HOME_TAB = '/home';
@@ -8,5 +9,7 @@ abstract class AppRoutes {
   static const BANNER_SCREEN = '/banner_screen';
   static const LOCATION_MAP = '/location_map';
 }
+=======
+>>>>>>> 0241349 (add routes)
 =======
 >>>>>>> 0241349 (add routes)
