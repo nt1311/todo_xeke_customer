7286b780117dea3e143574ecc45f063ed180b30f
Ngoc Tuan <hvntuan1311@gmail.com>
2023-08-03 01:47:00 +0700

    booking_car

--- lib/widget/widget_input_widget.dart
+++ lib/widget/widget_input_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_xeke_customer/config/constants/app_colors.dart';
 
 class InputWidget extends StatelessWidget {
   final String? hintText;
@@ -7,17 +8,25 @@ class InputWidget extends StatelessWidget {
   final bool readOnly;
   final VoidCallback? onTap;
 
-  const InputWidget(
-      {super.key, this.hintText, this.border, this.suffixIcon, this.readOnly = false, this.onTap});
+  const InputWidget({
+    super.key,
+    this.hintText,
+    this.border,
+    this.suffixIcon,
+    this.readOnly = false,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       onTap: onTap,
       readOnly: readOnly,
       decoration: InputDecoration(
         contentPadding: EdgeInsets.all(13),
         hintText: hintText,
+        hintStyle: TextStyle(color: NEUTRAL_GREY_3_COLOR),
+        // Đặt màu cho hintText tại đây
         suffixIcon: suffixIcon,
         fillColor: Colors.black26,
         border: border ??
