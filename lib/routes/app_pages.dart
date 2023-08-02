3462b51929d6d0857c0bc07a3201226e5aecf276
Ngoc Tuan <hvntuan1311@gmail.com>
2023-08-02 18:54:26 +0700

    booking_ca

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+import 'package:todo_xeke_customer/modules/booking_car/booking_car_screen.dart';
+import 'package:todo_xeke_customer/modules/main_menu_module/home_tab/home_tab_sceeen.dart';
+import 'package:todo_xeke_customer/modules/main_menu_module/main_menu_module.dart';
+part 'app_routes.dart';
+
+class AppPages {
+  static const initila = AppRoutes.MAIN_MENU;
+  static final routes = [
+    GetPage(
+      name: AppRoutes.MAIN_MENU,
+      page: () => const MainMenuScreen(),
+      binding: MainMenuBinding(),
+    ),
+    GetPage(
+        name: AppRoutes.HOME_TAB,
+        page: () => const HomeTabScreen(),
+        transition: Transition.cupertino),
+    GetPage(
+        name: AppRoutes.BOOKING_CAR,
+        page: () => const BookingCarScreen(),
+        transition: Transition.cupertino),
+  ];
+}
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,13 +1,9 @@
 part of "app_pages.dart";
 
 abstract class AppRoutes {
-  static const SPLASH = '/';
-  static const ONBOARDINGPAGE = '/onboardingpage';
-  static const LOCATION_ON = '/location on';
-  static const LOGIN_ACCOUNT = '/LOGIN ACCOUNT';
-  static const OTP = '/otp';
-  static const AUTH = "/auth";
-  static const PERSONAL_INFORMATION = '/personal information';
-  static const MAIN_MENU = '/main menu';
-  static const HOME = '/home';
+  static const HOME_TAB = '/home';
+  static const MAIN_MENU = '/main_menu';
+  static const BOOKING_CAR = '/booking_car';
+  static const BANNER_SCREEN = '/banner_screen';
+  static const GOOGLE_MAP = '/google_map';
 }
